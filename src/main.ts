6dc7b5929576831731de3176e329c7afa763d3eb
Chris Hernandez <hernandez@csdisco.com>
2019-10-30 14:30:53 -0500

    updates

--- src/main.ts
+++ src/main.ts
@@ -1,62 +1,16 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import {
-  ReposCreateStatusParams,
-  PullsListCommitsResponseItem,
-} from '@octokit/rest';
-
+import { ReposCreateStatusParams } from '@octokit/rest';
 import isSemanticMessage from './is-semantic-message';
 
-function commitsAreSemantic(commitItems: PullsListCommitsResponseItem[]) {
-  return commitItems.every(({ commit }) => isSemanticMessage(commit.message));
-}
-
-function canMergeCommit(commitItems) {
-  const ok = commitsAreSemantic(commitItems):
-  const message = ok ? 'Ready to create merge commit';
-  return { ok, message };
-}
-
-function createCreateStatus(
-  owner: string,
-  repo: string,
-  sha: string,
-  octokit: github.GitHub
-) {
-  return async (ok: boolean, message: string, context: string) => {
-    const status: ReposCreateStatusParams = {
-      owner,
-      repo,
-      sha,
-      state: ok ? 'success' : 'pending',
-      description: message,
-      context: `Conventional Pull Request - ${context}`,
-    };
-
-    await octokit.repos.createStatus(status);
-  };
-}
-
-function canSquashMerge(
-  title: string,
-  commitItems: PullsListCommitsResponseItem[]
-) {
-  const singleCommit = commitItems.length <= 1;
-  const titleOk = isSemanticMessage(title);
-  const commitsOk = commitsAreSemantic(commitItems);
-  const ok = singleCommit ? titleOk : commitsOk;
-
-  let message = '';
-
-  if (ok) {
-    message = 'Ready to be squashed and merged';
-  } else if (!ok && singleCommit) {
-    message = 'Make sure your commit is semantic';
+function getMessage(ok, singleCommit) {
+  if (!ok && singleCommit) {
+    return 'Make sure your commit message is semantic';
   } else if (!ok && !singleCommit) {
-    message = 'Make sure your PR title is semantic';
+    return 'Make sure your PR title is semantic';
   }
 
-  return { ok, message };
+  return 'Ready to be squashed and merged';
 }
 
 async function run() {
@@ -69,53 +23,29 @@ async function run() {
     const { allow_squash_merge, allow_merge_commit, allow_rebase_merge } = data;
     const pullRequest = github.context!.payload!.pull_request!;
     const { title, head, number: pull_number } = pullRequest;
-    const { data: commitItems } = await octokit.pulls.listCommits({
+    const { data: items } = await octokit.pulls.listCommits({
       owner,
       repo,
       pull_number,
     });
 
-    const createStatus = createCreateStatus(owner, repo, head.sha, octokit);
+    const singleCommit = items.length <= 1;
+    const ok = singleCommit
+      ? items.every(({ commit }) => isSemanticMessage(commit.message))
+      : isSemanticMessage(title);
 
-    if (allow_squash_merge) {
-      const { ok, message } = canSquashMerge(title, commitItems);
-      createStatus(ok, message, 'squash merging');
-    }
+    const message = getMessage(ok, singleCommit);
+    const status: ReposCreateStatusParams = {
+      owner,
+      repo,
+      sha: head.sha,
+      state: ok ? 'success' : 'pending',
+      description: message,
+      context: 'Conventional Squash Commits',
+    };
 
-    if (allow_merge_commit) {
-      const ok = commitsAreSemantic(commitItems);
-      const message = ok
-        ? 'Ready to create a merge commit or rebase and merge'
-        : 'Make sure all your commits are semantic';
-      const status: ReposCreateStatusParams = {
-        owner,
-        repo,
-        sha: head.sha,
-        state: ok ? 'success' : 'pending',
-        description: message,
-        context: 'Conventional Pull Request - squash merging',
-      };
-
-      await octokit.repos.createStatus(status);
-    }
-
-    if (allow_rebase_merge) {
-    }
-
-    // const isTitleSemantic = isSemanticMessage(title);
-    // const state = isTitleSemantic ? 'success' : 'pending';
-    // const description = 'this is the description';
-    // const status: ReposCreateStatusParams = {
-    //   owner,
-    //   repo,
-    //   state,
-    //   sha: head.sha,
-    //   description,
-    //   context: 'Conventional Pull Request',
-    // };
-    // const result = await octokit.repos.createStatus(status);
+    await octokit.repos.createStatus(status);
   } catch (error) {
-    // core.setFailed(error.message);
     console.log('err0r!', error);
   }
 }
