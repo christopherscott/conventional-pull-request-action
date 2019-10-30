5c30ba934578002f1290f0d14f45b2256805251e
Chris Hernandez <hernandez@csdisco.com>
2019-10-30 13:32:30 -0500

    try to get list of commits

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,20 @@ async function run() {
     if (!process.env.GITHUB_TOKEN) throw 'GITHUB_TOKEN env var required';
 
     const { owner, repo } = github.context.repo;
-    console.log(JSON.stringify(github.context.repo));
     const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
-    const reposInfo = await octokit.repos.get({ repo, owner });
-    console.log(JSON.stringify(reposInfo));
+    const { data } = await octokit.repos.get({ repo, owner });
+    const { allow_squash_merge, allow_merge_commit, allow_rebase_merge } = data;
 
-    // const { title, head } = github.context!.payload!.pull_request!;
+    const { title, head, number } = github.context!.payload!.pull_request!;
+
+    const commits = await octokit.pulls.listCommits({
+      owner,
+      repo,
+      pull_number: number,
+    });
+
+    console.log(JSON.stringify(commits, null, 2));
+
     // const isTitleSemantic = isSemanticMessage(title);
     // const state = isTitleSemantic ? 'success' : 'pending';
     // const description = 'this is the description';
