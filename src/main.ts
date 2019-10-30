e29f03fedb777bd91579d4b3d6849435e1213a67
Chris Hernandez <hernandez@csdisco.com>
2019-10-30 12:58:17 -0500

    log context

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,24 @@ import * as github from '@actions/github';
 import { ReposCreateStatusParams } from '@octokit/rest';
 import isSemanticMessage from './is-semantic-message';
 
+/**
+ * modes to support:
+ *
+ * - allow_squash_merge
+ * - allow_merge_commit
+ * - allow_rebase_merge
+ */
+
 async function run() {
   try {
     if (!process.env.GITHUB_TOKEN) throw 'GITHUB_TOKEN env var required';
 
     const { owner, repo } = github.context.repo;
+
+    console.log('---------------------------------');
+    console.log(JSON.stringify(github.context.repo));
+    console.log('---------------------------------');
+
     const { title, head } = github.context!.payload!.pull_request!;
     const isTitleSemantic = isSemanticMessage(title);
     const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
